271e9bc6eed16667798df544d3912e3588699f79
melody <liutmelody@gmail.com>
2019-11-01 12:58:59 -0600

    adding link for sufjan page

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='79m/templates')
+app = Flask(__name__, template_folder='79m/79m/templates')
 
 @app.route('/')
 def home_page():
-    # return render_template("test_file.html")
-    return "Hello world!"
+    return render_template("index.html")
+    # return "Hello world!"
+
+@app.route('/sufjan')
+def sufjan_is_a_liar():
+    return render_template("sufjan.html")
 
 if __name__ == '__main__':
     app.run(debug= True)
