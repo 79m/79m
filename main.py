520d232c812c8ad84c1b9117b125bcb061d7f278
79m <melody.liu@edu.uwaterloo.ca>
2019-11-01 13:11:53 -0600

    fixing broken template folder pt 2

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='79m/79m/templates')
+#app = Flask(__name__, template_folder='79m/79m/templates')
+app = Flask(__name__)
+
 
 @app.route('/')
 def home_page():
-    return render_template("templates/index.html")
+    return render_template("index.html")
     # return "Hello world!"
 
 @app.route('/sufjan')
 def sufjan_is_a_liar():
-    return render_template("templates/sufjan.html")
+    return render_template("sufjan.html")
 
 if __name__ == '__main__':
     app.run(debug= True)
