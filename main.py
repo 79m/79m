4a293c1db7462968b3437a1dbe786cd4546dbbb3
79m <melody.liu@edu.uwaterloo.ca>
2019-11-01 13:08:38 -0600

    fixing broken folder directory

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ app = Flask(__name__, template_folder='79m/79m/templates')
 
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    return render_template("templates/index.html")
     # return "Hello world!"
 
 @app.route('/sufjan')
 def sufjan_is_a_liar():
-    return render_template("sufjan.html")
+    return render_template("templates/sufjan.html")
 
 if __name__ == '__main__':
     app.run(debug= True)
