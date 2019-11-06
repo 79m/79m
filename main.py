8627f522bae3ea9d9b88511e1cc2f390006e71ee
79m <melody.liu@edu.uwaterloo.ca>
2019-11-06 17:42:45 -0600

    reverting

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='79m/79m/templates')
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home_page():
     return render_template("index.html")
     # return "Hello world!"
 
-@app.route('/sufjan')
-def sufjan_is_a_liar():
-    return render_template("sufjan.html")
+# @app.route('/sufjan')
+# def sufjan_is_a_liar():
+#     return render_template("sufjan.html")
 
 if __name__ == '__main__':
     app.run(debug= True)
